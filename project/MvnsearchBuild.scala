746351d26c4698ab627daa2b4d66c0cc1d9cc620
Toshiyuki Takahashi <t.toshi.0412@gmail.com>
2013-03-09 18:32:41 +0900

    Settings for publishing

--- project/MvnsearchBuild.scala
+++ project/MvnsearchBuild.scala
@@ -19,7 +19,42 @@ object MvnsearchBuild extends Build {
         "org.scalatest" %% "scalatest" % "1.9.1" % "test",
         "org.scala-sbt" % "command" % "0.12.2"
       )
-    )
+    ) ++ publishingSettings
   )
+
+  val publishingSettings = Seq(
+    publishMavenStyle := true,
+    publishTo <<= version { (v: String) => _publishTo(v) },
+    publishArtifact in Test := false,
+    pomExtra := _pomExtra
+  )
+
+  def _publishTo(v: String) = {
+    val nexus = "https://oss.sonatype.org/"
+    if (v.trim.endsWith("SNAPSHOT")) Some("snapshots" at nexus + "content/repositories/snapshots")
+    else Some("releases" at nexus + "service/local/staging/deploy/maven2")
+  }
+
+  val _pomExtra =
+    <url>http://github.com/tototoshi/mvnsearch</url>
+    <licenses>
+      <license>
+        <name>Apache License, Version 2.0</name>
+        <url>http://www.apache.org/licenses/LICENSE-2.0.html</url>
+        <distribution>repo</distribution>
+      </license>
+    </licenses>
+    <scm>
+      <url>git@github.com:tototoshi/mvnsearch.git</url>
+      <connection>scm:git:git@github.com:tototoshi/mvnsearch.git</connection>
+    </scm>
+    <developers>
+      <developer>
+        <id>tototoshi</id>
+        <name>Toshiyuki Takahashi</name>
+        <url>http://tototoshi.github.com</url>
+      </developer>
+    </developers>
+
 
 }
